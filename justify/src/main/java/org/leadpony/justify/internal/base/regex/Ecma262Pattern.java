6f03f99b8e50c25703d70ae506608cd89945b4ab
leadpony <hacker@leadpony.org>
2020-10-31 17:09:48 +0900

    Fix ecmascript regex

--- justify/src/main/java/org/leadpony/justify/internal/base/regex/Ecma262Pattern.java
+++ justify/src/main/java/org/leadpony/justify/internal/base/regex/Ecma262Pattern.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 the Justify authors.
+ * Copyright 2018, 2020 the Justify authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,12 +24,23 @@ import java.util.regex.Pattern;
  */
 public final class Ecma262Pattern {
 
+    /*
+     * Whitespace: U+0009, U+000B, U+000C, U+0020, U+0009, U+00a0, U+FEFF, and other Space_Separator(Zs)
+     * Line Terminator: U+000A, U+000D, U+2028, U+2029
+     */
+    private static final String WHITESPACE_CHARACTER = "\\h\\f\\n\\r\\u000b​\\u2028\\u2029\\ufeff";
+
+    private static final String WHITESPACE_CHARACTER_CLASS = "[" + WHITESPACE_CHARACTER + "]";
+
+    private static final String NON_WHITESPACE_CHARACTER_CLASS = "[^" + WHITESPACE_CHARACTER + "]";
+
     /**
      * Compiles the given regular expression into a pattern.
      *
      * @param regex the expression to be compiled
      * @return the regular expression compiled into a pattern
-     * @throws PatternSyntaxException if the syntax of the given expression is invalid
+     * @throws PatternSyntaxException if the syntax of the given expression is
+     *                                invalid
      */
     public static Pattern compile(String regex) {
         String translated = translate(regex);
@@ -39,7 +50,6 @@ public final class Ecma262Pattern {
     private static String translate(String regex) {
         StringBuilder builder = new StringBuilder();
         final int length = regex.length();
-        int last = 0;
         for (int i = 0; i < length; i++) {
             char c = regex.charAt(i);
             if (c == '\\') {
@@ -48,24 +58,28 @@ public final class Ecma262Pattern {
                     if (c == 'c') {
                         if (++i < length) {
                             c = regex.charAt(i);
-                            if ('a' <= c &&  c <= 'z') {
-                                builder.append(regex, last, i);
+                            if ('a' <= c && c <= 'z') {
+                                builder.append("\\c");
                                 builder.append((char) ('A' + c - 'a'));
-                                last = i + 1;
+                            } else {
+                                builder.append("\\c").append(c);
                             }
                         }
+                    } else if (c == 's') {
+                        builder.append(WHITESPACE_CHARACTER_CLASS);
+                    } else if (c == 'S') {
+                        builder.append(NON_WHITESPACE_CHARACTER_CLASS);
+                    } else {
+                        builder.append("\\").append(c);
                     }
+                } else {
+                    builder.append("\\");
                 }
             } else if (c == '$' && i + 1 >= length) {
-                builder.append(regex, last, i);
                 builder.append("\\z");
-                last = i + 1;
+            } else {
+                builder.append(c);
             }
-        }
-        if (last == 0) {
-            return regex;
-        } else if (last < length) {
-            builder.append(regex, last, length);
         }
         return builder.toString();
     }
